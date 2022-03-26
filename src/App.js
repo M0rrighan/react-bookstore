a10fa4f622136e3362c59c9ad5b9bb58e9b8c78c
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-03-26 20:48:24 +0100

    Install react router dom; edit App.js: add routes for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+
 import './App.css';
 
-function App() {
-  return (
+const App = () => (
+  // <Router basename="/react-bookstore">
+  <Router>
     <div>
-      APP
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Books />}
+        />
+        <Route
+          path="/Categories"
+          element={<Categories />}
+        />
+      </Routes>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
