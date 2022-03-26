259d52db20151cfad080b5856c51880bdbb37a5d
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-03-26 20:22:30 +0100

    Add Pages: Categoris, Books

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import NewBook from '../components/NewBookForm';
+import BookList from '../components/BookList';
 
 function Books() {
   return (
-    <div>Books</div>
+    <div>
+      <BookList />
+      <NewBook />
+    </div>
   );
 }
 
