5f4aff91dc9f6cb9e92a951958ab526442400a34
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-03-30 18:22:59 +0200

    Add funcionality for Add book submit button

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import randomId from 'random-id';
+import { addBook } from '../redux/books/books';
 
 function NewBookForm() {
+  const dispatch = useDispatch();
   const categories = [
     'Action and adventure',
     'Alternate history',
@@ -38,16 +42,58 @@ function NewBookForm() {
     </option>
   ));
 
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: randomId(),
+      title,
+      author,
+      category,
+      progressPercent: Math.floor(Math.random() * 101),
+      chapter: 'Not Updated',
+    };
+
+    // dispatch an action and pass it the newBook object
+    dispatch(addBook(newBook));
+    // reset input fields
+    setTitle('');
+    setAuthor('');
+  };
+
+  const updateTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const updateAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+  const updateCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
   return (
     <div>
       <h2>ADD NEW BOOK</h2>
-      <form>
+      <form onSubmit={submitBookToStore}>
         <input
           type="text"
           placeholder="Book titile"
+          onChange={updateTitle}
+          value={title}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          onChange={updateAuthor}
+          value={author}
           required
         />
         <select
+          onChange={updateCategory}
           defaultValue="Unset"
           required
         >
