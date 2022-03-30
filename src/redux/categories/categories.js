c6015bcfc06d8076b863d95972fca991a66601ab
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-03-30 16:12:19 +0200

    Export default categoriesReducer: state changes for CHECK_STATUS action

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -6,3 +6,15 @@ export const checkStatus = (payload) => ({
   type: CHECK_STATUS,
   payload,
 });
+
+const categoriesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Construction';
+
+    default:
+      return state;
+  }
+};
+
+export default categoriesReducer;
