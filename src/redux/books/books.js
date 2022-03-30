4df1f9462f78da98ff8fe8a3db0f163e81591971
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-03-30 16:03:02 +0200

    Export default booksReducer: state changes for adding and removing book actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -11,3 +11,17 @@ export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
   payload,
 });
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id);
+
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
